0805fed43db05104ec66cc89ac7b4ff6936cb529
Takuya Matsumoto <tmatsnj@gmail.com>
2023-01-25 16:53:33 -0500

    got the data rendering seperately for the roadmap routes

--- controllers/roadmapRoutes.js
+++ controllers/roadmapRoutes.js
@@ -1,9 +1,57 @@
 const router = require('express').Router();
 const { Feedback, User, Comment, Reply } = require('../models');
+const { Op } = require("sequelize");
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-    res.render('roadmap');
+    Feedback.findAll({
+        where: {
+            [Op.or]: [
+                { status: 'planned' },
+                { status: 'live' },
+                { status: 'in-progress' }
+            ]
+        },
+        attributes: [ 'id', 'title', 'category', 'upvotes', 'status', 'description'],
+            include: [{
+                model: Comment,
+                attributes: [ 'id', 'content', 'feedback_id', 'user_id' ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(feedbackData => {
+        const planned = feedbackData.map(feedback => {
+            if (feedback.status === 'planned') {
+                return feedback.get({ plain: true })
+            }
+        });
+
+        const live = feedbackData.map(feedback => {
+            if (feedback.status === 'live') {
+                return feedback.get({ plain: true })
+            }
+        });
+
+        const inProgress = feedbackData.map(feedback => {
+            if (feedback.status === 'in-progress') {
+                return feedback.get({ plain: true })
+            }
+        });
+
+        res.render('roadmap', { planned, live, inProgress, logged_in: req.session.logged_in });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
 });
 
 module.exports = router;
