0c932af4d9059ad3ffc9279065b9e9dd9976a808
Takuya Matsumoto <tmatsnj@gmail.com>
2023-02-08 21:25:57 -0500

    got the header styling done for the sign up page

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const roadmapRoutes = require('./roadmapRoutes');
 
+router.use('/api', apiRoutes);
 router.use('/roadmap', roadmapRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
