840ca5200a5b8fdfa5235fc1ffda685537b590ce
Takuya Matsumoto <tmatsnj@gmail.com>
2023-04-03 20:28:04 -0400

    got the tablet view done for the homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const feedbackRoutes = require('./feedbackRoutes');
 const commentRoutes = require('./commentRoutes');
+// const replyRoutes = require('./replyRoutes');
 
 router.use('/users', userRoutes);
 router.use('/feedbacks', feedbackRoutes);
 router.use('/comments', commentRoutes);
+// router.use('/replies', replyRoutes);
 
 module.exports = router;
