c58ff5c8fbfe018620cf01e71e92c5d885957896
Takuya Matsumoto <tmatsnj@gmail.com>
2023-04-06 14:32:21 -0400

    got the no feedback page to render correctly in mobile view

--- public/js/burgermenu.js
+++ public/js/burgermenu.js
@@ -1,20 +1,11 @@
-// Get the burger menu icon element
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerMenuLines = document.querySelectorAll('.burger-menu-line');
-
-// Get the "main__sidebar-category" and "main__sidebar-roadmap" containers
 const categoryContainer = document.querySelector('.main__sidebar-category');
 const roadmapContainer = document.querySelector('.main__sidebar-roadmap');
 const sideMenu = document.querySelector('.side-menu');
 
-
-console.log(categoryContainer);
-// Add a click event listener to the burger menu icon
 burgerMenu.addEventListener('click', () => {
-  // Toggle the "active" class on the burger menu icon
     burgerMenu.classList.toggle('active');
-    
-    // Toggle the visibility of the "main__sidebar-category" and "main__sidebar-roadmap" containers
     categoryContainer.classList.toggle('active');
     roadmapContainer.classList.toggle('active');
     sideMenu.classList.toggle('show');
