72af444c207acdfe6867a9f5fef60f08b7e75955
Takuya Matsumoto <tmatsnj@gmail.com>
2023-04-17 16:53:38 -0400

    reverted back to the old server.js file structure

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const uri = process.env.URI
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,26 +24,6 @@ const sess = {
     })
 };
 
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
-
-
 app.use(session(sess))
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -56,7 +34,7 @@ app.use(express.urlencoded({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.resolve(__dirname, '../build')));
+
 app.use(routes);
 
 sequelize.sync({
