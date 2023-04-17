f4fe1e29a9ef062f2592baf757a09cf86b9d9008
Takuya Matsumoto <tmatsnj@gmail.com>
2023-04-17 17:25:55 -0400

    implemented JAWSDB credentials

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,14 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+    } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASSWORD, {
-            host: 'localhost',
+            host: process.env.DB_HOST || 'localhost',
             dialect: 'mysql',
-            port: 3306
+            port: process.env.DB_PORT || 3306
         }
     );
 };
