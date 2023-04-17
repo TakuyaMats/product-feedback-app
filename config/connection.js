2e869c061e46f9beb2ae67269eb5947f84b5f14b
Takuya Matsumoto <tmatsnj@gmail.com>
2023-04-17 16:43:49 -0400

    Retrying deployment code - Attempt 3

--- config/connection.js
+++ config/connection.js
@@ -17,4 +17,13 @@ if (process.env.MONGODB_URI) {
     );
 };
 
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 module.exports = sequelize;
